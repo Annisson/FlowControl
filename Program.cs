e0c0afacd85c2998de9e6332d0668256a89efdb9
Annica Andersson <annicaandersson@live.se>
2024-03-02 22:26:39 +0100

    Add do/while to loop menu

--- Program.cs
+++ Program.cs
@@ -6,22 +6,33 @@
         {
             Console.WriteLine("Flow Control Application");
 
-            Console.WriteLine("Main menu, please enter a number from the options below to continue.");
-            Console.WriteLine("0. Exit");
+            bool runMenu = true;
 
-            int userInput = int.Parse(Console.ReadLine());
-
-            switch (userInput)
+            do
             {
-                case 0: //Exit the application
-                    Environment.Exit(0);
-                    break;
+                Console.WriteLine("Main menu, please enter a number from the options below to continue.");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Rerun menu");
 
+                int userInput = int.Parse(Console.ReadLine());
 
-                default:
-                    Console.WriteLine("Incorrect input, please use numbers 0 or 1 only.");
-                    break;
-            }
+                switch (userInput)
+                {
+                    case 0: //Exit the application
+                        runMenu = false;
+                        Environment.Exit(0);
+                        break;
+
+                        case 1:
+                            continue;
+
+                    default:
+                        Console.WriteLine("Incorrect input, please use numbers 0 or 1 only.");
+                        break;
+                }
+
+            } while (runMenu);
+
 
         }
     }
