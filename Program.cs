e04b30804e264a6b1801416ef9124f5ae9d4862a
Annica Andersson <annicaandersson@live.se>
2024-03-02 22:22:32 +0100

    Add switch shell and userinput for menu

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Flow Control");
+            Console.WriteLine("Flow Control Application");
+
+            Console.WriteLine("Main menu, please enter a number from the options below to continue.");
+            Console.WriteLine("0. Exit");
+
+            int userInput = int.Parse(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 0: //Exit the application
+                    Environment.Exit(0);
+                    break;
+
+
+                default:
+                    Console.WriteLine("Incorrect input, please use numbers 0 or 1 only.");
+                    break;
+            }
+
         }
     }
 }
