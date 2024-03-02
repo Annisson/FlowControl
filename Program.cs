0478cd55aa02902252de8bcb11c46dcafe004a20
Annica Andersson <annicaandersson@live.se>
2024-03-02 22:46:02 +0100

    Add switch shell for all menu options

--- Program.cs
+++ Program.cs
@@ -12,7 +12,10 @@
             {
                 Console.WriteLine("Main menu, please enter a number from the options below to continue.");
                 Console.WriteLine("0. Exit");
-                Console.WriteLine("1. Rerun menu");
+                Console.WriteLine("1. Cinema, age-check");
+                Console.WriteLine("2. Cinema, number of guests and total cost");
+                Console.WriteLine("3. Repeat 10 times");
+                Console.WriteLine("4. Play a word-game");
 
                 int userInput = int.Parse(Console.ReadLine());
 
@@ -23,8 +26,21 @@
                         Environment.Exit(0);
                         break;
 
-                        case 1:
-                            continue;
+                    case 1:
+                        Console.WriteLine("Cinema, age-check");
+                        continue;
+
+                    case 2:
+                        Console.WriteLine("Cinema, number of guests and total cost");
+                        continue;
+
+                    case 3:
+                        Console.WriteLine("Repeat 10 times");
+                        continue;
+
+                    case 4:
+                        Console.WriteLine("Play a word-game");
+                        continue;
 
                     default:
                         Console.WriteLine("Incorrect input, please use numbers 0 or 1 only.");
