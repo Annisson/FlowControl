806f95e6007c4910920fa76baf8cf5f8a57b0409
Annica Andersson <annicaandersson@live.se>
2024-03-05 15:30:28 +0100

    Add errorhandling for incorrect input values

--- Cinema.cs
+++ Cinema.cs
@@ -46,44 +46,50 @@ namespace FlowControl
             int price = 0;
 
             Console.Write("Welcome to the cinema!\nPlease enter your age to check the price for your ticket: ");
-            int.TryParse(Console.ReadLine(), out int input); //Parsing the string input into an int
 
-            if (input < 20)
+            if (int.TryParse(Console.ReadLine(), out int input)) //Parsing the string input into an int
             {
-                if (input < 0)
+                if (input < 20)
                 {
-                    Console.WriteLine("The age cannot be less than 0");
+                    if (input < 0)
+                    {
+                        Console.WriteLine("The age cannot be less than 0");
+                    }
+                    else if (input < 5)
+                    {
+                        Console.WriteLine("Children under the age of 5 enter for free");
+                    }
+                    else //Age 5 and up
+                    {
+                        price = CinemaPrices(input); //Caling the method which contains the prices and presenting the return value below
+                        Console.WriteLine($"Youth ticket price: {price} SEK");
+                    }
                 }
-                else if (input < 5)
+                else if (input >= 20)
                 {
-                    Console.WriteLine("Children under the age of 5 enter for free");
+                    if (input >= 20 && input <= 64)
+                    {
+                        price = CinemaPrices(input);
+                        Console.WriteLine($"Standard ticket price: {price} SEK");
+                    }
+                    else if (input > 64 && input <= 100)
+                    {
+                        price = CinemaPrices(input);
+                        Console.WriteLine($"Pensioner ticket price: {price} SEK");
+                    }
+                    else //Above age 100, meaning age 101 and up
+                    { 
+                        Console.WriteLine("Pensioners over the age of 100 enter for free");
+                    }
                 }
-                else //Age 5 and up
+                else
                 {
-                    price = CinemaPrices(input); //Caling the method which contains the prices and presenting the return value below
-                    Console.WriteLine($"Youth ticket price: {price} SEK");
-                }
-            }
-            else if (input >= 20)
-            {
-                if (input >= 20 && input <= 64)
-                {
-                    price = CinemaPrices(input);
-                    Console.WriteLine($"Standard ticket price: {price} SEK");
-                }
-                else if (input > 64 && input <= 100)
-                {
-                    price = CinemaPrices(input);
-                    Console.WriteLine($"Pensioner ticket price: {price} SEK");
-                }
-                else //Above age 100, meaning age 101 and up
-                { 
-                    Console.WriteLine("Pensioners over the age of 100 enter for free");
+                    Console.WriteLine("Error beep boop");
                 }
             }
             else
             {
-                Console.WriteLine("Error beep boop");
+                Console.WriteLine("The age must be entered with numbers only");
             }
         }
 
@@ -93,20 +99,51 @@ namespace FlowControl
             int totalPrice = 0;
 
             Console.Write("Welcome to the cinema!\nPlease enter how many tickets you would like to buy: ");
-            int.TryParse(Console.ReadLine(), out int numberOfTickets); //Saving the string user input into an int by parsing
-            int[] ticketArray = new int[numberOfTickets]; //User input value is used to create the array, if the user asks for 5 tickets, then the numberOfTickets variable will set the array size to 5
-
-            Console.Write("\nPlease enter the age of each person you are buying a ticket for. ");
-
-            foreach (int ticket in ticketArray) //Looping through the value the user chose, asking for the age of each person 
+            if (int.TryParse(Console.ReadLine(), out int numberOfTickets)) //Saving the string user input into an int by parsing
             {
-                Console.Write("\nAge: ");
-                int.TryParse(Console.ReadLine(), out int ageInput);
-                pricePerPerson = CinemaPrices(ageInput); //Calling the CinemaPrices method using the age and getting the price in return
-                totalPrice += pricePerPerson; //Adding each price input to the totalPrice variable
+                int[] ticketArray = new int[numberOfTickets]; //User input value is used to create the array, if the user asks for 5 tickets,
+                                                              //then the numberOfTickets variable will set the array size to 5
+
+                Console.Write("\nPlease enter the age of each person you are buying a ticket for. ");
+
+                foreach (int ticket in ticketArray) //Looping through the value the user chose, asking for the age of each person 
+                {
+                    Console.Write("\nEnter the age for one person, and press Enter: ");
+                    if (int.TryParse(Console.ReadLine(), out int ageInput))
+                    {
+                        pricePerPerson = CinemaPrices(ageInput); //Calling the CinemaPrices method using the age and getting the price in return
+                        totalPrice += pricePerPerson; //Adding each price input to the totalPrice variable
+                    }
+                    else
+                    {
+                        Console.WriteLine("The age must be entered with numbers only. Please try again");
+                        bool incorrectInput = true; //Counting on more than 1 incorrect input, will run do-while until the input is correct
+                        do
+                        {
+                            Console.Write("\nEnter the age for one person, and press Enter: ");
+                            if (int.TryParse(Console.ReadLine(), out int ageInputRetry))
+                            {
+                                pricePerPerson = CinemaPrices(ageInputRetry); //Calling the CinemaPrices method using the age and getting the price in return
+                                totalPrice += pricePerPerson; //Adding each price input to the totalPrice variable
+                                incorrectInput = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("The age must be entered with numbers only");
+                            }
+
+                        } while (incorrectInput);
+
+                    }
+                    
+                }
+                Console.WriteLine("Number of tickets: " + numberOfTickets.ToString());
+                Console.WriteLine("Total price for all tickets: " + totalPrice);
             }
-            Console.WriteLine("Number of tickets: " + numberOfTickets.ToString());
-            Console.WriteLine("Total price for all tickets: " + totalPrice);
+            else
+            {
+                Console.WriteLine("The number of tickets must be entered with numbers only");
+            }
 
         }
     }
